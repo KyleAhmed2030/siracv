0ee1b2603582bdd65142814d68dcaf52befc661f
kyleahmed1993 <41274789-kyleahmed1993@users.noreply.replit.com>
2025-04-20 13:06:21 +0000

    Restored to '87847ee9bbc2e0364dbc9f640079d981722db04e'
    Replit-Restored-To: 87847ee9bbc2e0364dbc9f640079d981722db04e

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { I18nextProvider } from 'react-i18next';
-import { Analytics } from '@vercel/analytics/react';
-import i18n from './localization/i18n';
-import App from './App';
+import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 
-// Create a root
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-// Render app
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-      <Analytics />
-    </I18nextProvider>
+    <App />
   </React.StrictMode>
 );
